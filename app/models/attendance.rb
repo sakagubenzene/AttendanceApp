7d4a28327d9259cbecd2ce4f475e1ec87eb183b3
sakagubenzene <65774562+sakagubenzene@users.noreply.github.com>
2024-04-16 07:24:36 +0000

    Add message and attendance  model

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,3 +1,8 @@
 class Attendance < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, presence: true
+  validates :begin_at, presence: true
+  validates :finish_at, presence: true
+  validate :finish_time_after_begin_time
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :sender, class_name: 'User'
   belongs_to :receiver, class_name: 'User'
+  default_scope -> { order(created_at: :desc) }
+
+  validates :sender_id, presence: true
+  validates :receiver_id, presence: true
+  validates :content, presence: true, length: { minimum: 1, maximum: 100 }
+  validate :content_must_include_specific_word
+
+  private
+    def content_must_include_specific_word
+      unless content.include?("ありがとう")
+        errors.add(:content, "「ありがとう」という言葉を含めてください！")
+      end
+    end
 end
