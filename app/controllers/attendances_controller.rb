57f1f5208a6a5650b74d0e7bd003602c52ff9a16
sakagubenzene <65774562+sakagubenzene@users.noreply.github.com>
2024-05-06 09:33:01 +0000

    できないなあ

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -11,7 +11,13 @@ class AttendancesController < ApplicationController
   end
 
   def index
-    @attendances = Attendance.where(status:"modification_request").all.includes(user: :messages)
+    @q = Attendance.where(status: "begin").ransack(params[:q])
+    @attendances = Kaminari.paginate_array(@q.result).page(params[:page])
+    begin_tupples= Attendance.order(:user_id).where(status: "begin")
+    finish_tupples= Attendance.order(:user_id).where(status: "finish")
+    begin_tupples.zip(finish_tupples) do |b, f|
+      if 
+    end
   end
 
   def create
